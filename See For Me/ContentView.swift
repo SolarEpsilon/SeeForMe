3cd5f538ffaab186c3ade7af8c15b04ee6f478fc
Ronan M <solarepsilon@gmail.com>
2025-07-07 08:16:36 -0500

    Initial Commit

---
+++ See For Me/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  See For Me
+//
+//  Created by Ronan M on 7/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ See For Me/See_For_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  See_For_MeApp.swift
+//  See For Me
+//
+//  Created by Ronan M on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct See_For_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
